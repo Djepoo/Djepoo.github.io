7e4b9f43c02fe51fc0d8548f6d59121b46e6daaf
Djepoo <rejep.pehlivanov@abv.bg>
2016-07-30 22:53:47 +0300

    Web site
    This is my simple web site!

---
+++ HTML AND CSS/rezultat.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<head>
+ <title>Rezultat</title>
+<head>
+<body>
+<html>
+<body>
+
+Welcome <?php echo $_POST["Username"]; ?><br>
+Your pass is: <?php echo $_POST["Password"]; ?>
+
+</body>
+</html>
+
+
+</body>
+</html>
